1558bc79f8fa100889611b2d9ee84f901bba28f6
Leandro Pessoa <leandropessoareis@gmail.com>
2025-10-20 23:35:35 -0300

    fix(backend): change the database connection string

--- backend/src/db/connectDatabase.ts
+++ backend/src/db/connectDatabase.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDatabase = async (): Promise<mongoose.Connection | undefined> => {
 	try {
-		mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.cjn95ex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+		mongoose.connect(process.env.DATABASE_CONNECTION_STRING ?? '')
 		return mongoose.connection
 	} catch(err) {
 		console.log(err)
