6ee10031d1c1b90e2cc36027245e63b4d1c4ddc7
Leandro Pessoa <leandropessoareis@gmail.com>
2025-10-22 22:40:17 -0300

    docs(backend): add comments in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import { connectDatabase } from './db/connectDatabase.js'
 import routes from './routes/index.js'
 
+// conexão com o banco de dados
 const connect = await connectDatabase()
 connect?.once('open', () => { console.log('Conectado ao banco de dados')})
 
 const app = express()
 
+// rotas
 routes(app)
 
 export default app
