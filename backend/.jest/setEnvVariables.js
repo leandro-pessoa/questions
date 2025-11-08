2df99632c4b7fb387e956b702d77046957d14649
Leandro Pessoa <leandropessoareis@gmail.com>
2025-11-07 23:10:22 -0300

    chore(backend): set env vars for jest

---
+++ backend/.jest/setEnvVariables.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-undef
+process.env.DATABASE_CONNECTION_STRING = 'mongodb+srv://leandropessoareis:NlZI0QNEVpvs6mAr@cluster0.cjn95ex.mongodb.net/questions?retryWrites=true&w=majority&appName=Cluster0'
