de37f5764afee9a360a23537eb0a54288bcf7c45
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-07 17:02:19 -0300

    feat(frontend): create initial styles variables

---
+++ frontend/src/styles/vars.ts
@@ -0,0 +1,24 @@
+export const vars = {
+	fonts: {
+		primaryFont: '"Roboto", sans-serif'
+	},
+
+	colours: {
+		white: '#fff',
+		black: '#0A0E14',
+		darkGray: '#48536B',
+		gray: '#ccc',
+		lightGray: '#F7FBFF',
+		darkBlue: '#0C1018',
+		blue: '#027AF2',
+		red: '#ec4a4a'
+	},
+
+	breakpoints: {
+		desktop: '1280px',
+		notebook: '1024px',
+		tablet: '869px',
+		smartphone: '669px',
+		smallSmartphone: '429px',
+	}
+}
