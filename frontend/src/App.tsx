d16467b08659ce187293227c5bad97b2c75c1234
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-07 17:58:46 -0300

    feat: add theme provider to the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import AppRoutes from './routes'
 import { GlobalStyles } from './components/GlobalStyles'
+import { ThemeProvider } from 'styled-components'
+import { useAppSelector } from './app/hooks'
+import { selectTheme } from './app/reducers/theme'
+import { lightTheme, darkTheme } from './styles/themeVars'
 
 const App = () => {
+	const theme = useAppSelector(selectTheme)
+
 	return (
-		<>
+		<ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
 			<GlobalStyles />
 			<AppRoutes />
-		</>
+		</ThemeProvider>
 	)
 }
 
