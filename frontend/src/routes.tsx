1910d3f1ffae2640b069caf684b87bf6e2bdbe2e
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-09 22:46:59 -0300

    feat(frontend): add login page route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultPage from './pages/DefaultPage'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 const AppRoutes = () => {
 	return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
 			<Routes>
 				<Route path='/' element={ <DefaultPage /> }>
 					<Route index element={ <Home /> }/>
+					<Route path='login' element={ <Login /> }/>
 				</Route>
 			</Routes>
 		</BrowserRouter>
