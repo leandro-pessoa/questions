3db9be25951fdcacdd668e99216b08224649b189
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-11 00:01:26 -0300

    feat(components): create small component

---
+++ frontend/src/components/Small/index.tsx
@@ -0,0 +1,22 @@
+import { vars } from '@/styles/vars'
+import styled from 'styled-components'
+
+interface ISmallProps {
+    readonly $error?: boolean
+}
+
+// componente geral de small
+// props: error
+export const Small = styled.small<ISmallProps>`
+    // props
+    // caso seja true, ficará vermelho
+    color: ${({ $error = false }) => $error ? vars.colors.red : '#000'};
+    font-size: .9rem;
+    display: block;
+    max-width: 400px;
+
+
+    @media screen and (min-width: ${vars.breakpoints.smartphone}) {
+        font-size: 1rem;
+    }
+`
