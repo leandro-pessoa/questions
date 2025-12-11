946956eb2ae7f39ccce0a84d06d88a63b97bb6c2
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-11 00:00:50 -0300

    feat(pages): divide login page styles to other file

---
+++ frontend/src/pages/Login/styles.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { vars } from '@/styles/vars'
+import { flex } from '@/utils/flex'
+
+const xPadding = 16
+
+export const LoginContainer = styled.main`
+	${flex('column', '', '', '32px')}
+	background-color: transparent;
+	backdrop-filter: blur(10px);
+	padding: 16px;
+	border: 1px solid ${(props) => props.theme.colors.primaryBorderColor};
+	border-radius: ${vars.border.radius};
+	height: 70%;
+	width: calc(80% - ${xPadding * 2}px);
+
+	@media screen and (min-width: ${vars.breakpoints.smallSmartphone}) {
+		width: 250px;
+	}
+	@media screen and (min-width: ${vars.breakpoints.tablet}) {
+		width: 300px;
+		padding: 32px;
+	}
+`
