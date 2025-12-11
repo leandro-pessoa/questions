88812632088ea7aa3df141d514185c9afdb8b066
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-11 00:05:01 -0300

    feat(components): improve input with errors, global padding and hover

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,13 +1,24 @@
 import { vars } from '@/styles/vars'
 import styled from 'styled-components'
 
-const Input = styled.input`
-	background-color: ${(props) => props.theme.colors.secondaryBackgroundColor};
-	border: 1px solid ${(props) => props.theme.colors.primaryBorderColor};
+interface IInputProps {
+	readonly $error?: boolean
+}
+
+const xPadding = 14
+
+const Input = styled.input<IInputProps>`
+	background-color: ${(props) => props.theme.colors.primaryBackgroundColor};
+	border: 1px solid ${(props) => props.$error ? vars.colors.red : props.theme.colors.primaryBorderColor};
 	color: ${(props) => props.theme.colors.primaryFontColor};
 	border-radius: ${vars.border.radius};
-	padding: 6px 14px;
+	padding: 6px ${xPadding}px;
 	transition: ease .1s;
+	width: calc(100% - ${xPadding * 2}px);
+
+	&:hover {
+		border-color: ${vars.colors.mediumGray};
+	}
 
 	&:focus {
 		outline: 3px solid ${vars.colors.blue}aa;
