0417087f0e9f0e51a85bfef6bb618629399f1fd1
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-13 21:15:24 -0300

    chore(backend): add helmet on the express app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import routes from './routes/index'
 import errors from './middlewares/errors'
 import cors from 'cors'
 import { corsOptions } from './config/corsOptions'
+import helmet from 'helmet'
 
 // conexão com o banco de dados
 connectDatabase().then(() => { console.log('Conectado ao banco de dados')})
@@ -12,6 +13,9 @@ const app = express()
 
 // utilização do cors em todas as rotas
 app.use(cors(corsOptions))
+
+// helmet, para headers mais seguros
+app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' }}))
 
 // rotas
 routes(app)
