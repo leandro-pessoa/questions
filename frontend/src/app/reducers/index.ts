712735449a3a394fc63567054e617ff70ffaaa9e
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-15 23:40:17 -0300

    feat(frontend): add user reducer on the store

--- frontend/src/app/reducers/index.ts
+++ frontend/src/app/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import themeReducer from './theme'
 import loadingReducer from './loading'
+import userReducer from './user'
 
 export const rootReducer = combineReducers({
 	theme: themeReducer,
-	loading: loadingReducer
+	loading: loadingReducer,
+	user: userReducer
 })
