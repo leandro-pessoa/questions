50fcf347a9ad3f11a32c864391f71b61361bbc82
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-15 23:39:47 -0300

    feat(frontend): add user/token global states and yours respectives se actions

--- frontend/src/app/reducers/user.ts
+++ frontend/src/app/reducers/user.ts
@@ -1,15 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+import type { IUser } from '@/types/IUser'
+import type { RootState } from '../store'
 
-const initialState = {
+interface IUserState {
+	user: IUser | null
+	token: string
+}
 
+const initialState: IUserState = {
+	user: null,
+	token: ''
 }
 
 const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-
+		setUser: (state, action: PayloadAction<IUser>) => {
+			state.user = action.payload
+		},
+		setToken: (state, action: PayloadAction<IUserState['token']>) => {
+			state.token = action.payload
+		}
 	}
 })
 
 export default userSlice.reducer
+
+export const { setUser, setToken } = userSlice.actions
+
+export const selectUser = (state: RootState) => state.user.user
+export const selectToken = (state: RootState) => state.user.token
