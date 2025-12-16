1fec689e73ff6b4d16a505f172b6cf58aada1810
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-15 23:38:15 -0300

    chore(frontend): create IUser type

---
+++ frontend/src/types/IUser.ts
@@ -0,0 +1,11 @@
+import type { IAnsweredQuestion } from './IAnsweredQuestion'
+
+export interface IUser {
+	_id: string
+	role: 'default' | 'admin'
+	completeName: string
+	email: string
+	answeredQuestions?: IAnsweredQuestion[]
+	passwordHash: string
+	password: string
+}
