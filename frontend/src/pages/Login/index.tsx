af6155e21a3e5c2d1d35d67f35ffbeab3429b45c
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-17 23:57:42 -0300

    fix(pages): improve login page with new components rules and add hr

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -11,13 +11,14 @@ import FormInput from '@/components/Input/FormInput'
 import { Title } from '@/components/Title'
 import { LogIn } from 'lucide-react'
 import Form from '@/components/Form'
-import { LoginContainer } from './styles'
+import { Container } from '@/components/Container'
 import InputContainer from '@/components/Input/InputContainer'
 import { StyledLink } from '@/components/Link'
 import { Small } from '@/components/Small'
 import ThemeButton from '@/components/Button/ThemeButton'
 
 import type { FieldValues } from 'react-hook-form'
+import { Hr } from '@/components/Hr'
 
 const Login = () => {
 	const dispatch = useAppDispatch()
@@ -31,35 +32,58 @@ const Login = () => {
 			dispatch(setUser(res.data.user))
 			dispatch(setToken(res.data.token))
 			navigate('/')
-		} catch(err) {
+		} catch (err) {
 			axiosError(err)
 		}
 		dispatch(setIsLoading(false))
 	}
 
 	return (
-		<CenterContainer>
-			<ThemeButton fixed={true}/>
-			<LoginContainer>
+		<CenterContainer absolutePosition={true}>
+			<ThemeButton fixed={true} />
+			<Container $fixedWidth={true}>
 				<Title>Entrar</Title>
 				<Form onSubmit={submitHandle}>
 					<InputContainer>
 						<label htmlFor="email">E-mail</label>
-						<FormInput id='email' name='E-mail' email={true} required/>
+						<FormInput
+							id="email"
+							name="E-mail"
+							email={true}
+							required
+						/>
 					</InputContainer>
 					<InputContainer>
 						<label htmlFor="password">Senha</label>
-						<FormInput id='password' name='Senha' type='password' patternsOption={false} required/>
+						<FormInput
+							id="password"
+							name="Senha"
+							type="password"
+							patternsOption={false}
+							required
+						/>
 					</InputContainer>
-					<Small style={{textAlign: 'center', marginTop: '8px'}}>
-						Esqueceu sua senha? <StyledLink to='/' decoration={true}>Altere aqui</StyledLink>
+					<Small style={{ textAlign: 'center', marginTop: '8px' }}>
+						Esqueceu sua senha?{' '}
+						<StyledLink to='/' decoration={true}>
+							Altere aqui
+						</StyledLink>
 					</Small>
-					<Button onClick={() => {}} style={{marginTop: '16px'}} type='submit'>
+					<Hr />
+					<Small style={{ textAlign: 'center' }}>
+						<StyledLink to='/usuario/cadastro' decoration={true}>
+							Cadastre-se aqui
+						</StyledLink>
+					</Small>
+					<Button
+						style={{ marginTop: '16px' }}
+						type="submit"
+					>
 						<LogIn />
 						Entrar
 					</Button>
 				</Form>
-			</LoginContainer>
+			</Container>
 		</CenterContainer>
 	)
 }
