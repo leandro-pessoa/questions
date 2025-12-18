ddf2ced9971c50a905375af3978a9051243c0bac
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-17 23:54:01 -0300

    fix(components): create separate container

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -2,9 +2,13 @@ import styled from 'styled-components'
 import { vars } from '@/styles/vars'
 import { flex } from '@/utils/flex'
 
+interface IContainerProps {
+	readonly $fixedWidth?: boolean
+}
+
 const xPadding = 16
 
-export const LoginContainer = styled.main`
+export const Container = styled.main<IContainerProps>`
 	${flex('column', '', '', '32px')}
 	background-color: transparent;
 	backdrop-filter: blur(10px);
@@ -13,11 +17,15 @@ export const LoginContainer = styled.main`
 	border-radius: ${vars.border.radius};
 	width: calc(80% - ${xPadding * 2}px);
 
-	@media screen and (min-width: ${vars.breakpoints.smallSmartphone}) {
-		width: 250px;
-	}
-	@media screen and (min-width: ${vars.breakpoints.tablet}) {
-		width: 300px;
-		padding: 32px;
-	}
+	${({ $fixedWidth = false }) =>
+		$fixedWidth &&
+		`
+		@media screen and (min-width: ${vars.breakpoints.smallSmartphone}) {
+			width: 250px;
+		}
+		@media screen and (min-width: ${vars.breakpoints.tablet}) {
+			width: 300px;
+			padding: 32px;
+		}
+	`}
 `
