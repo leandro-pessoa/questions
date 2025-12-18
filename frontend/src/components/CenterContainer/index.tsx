c726e790f514cf1e99d337e713f8935982faebb1
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-18 00:01:56 -0300

    feat(components): adapt CenterContainer to non absolute position

--- frontend/src/components/CenterContainer/index.tsx
+++ frontend/src/components/CenterContainer/index.tsx
@@ -1,11 +1,28 @@
 import styled from 'styled-components'
 import { flex } from '@/utils/flex'
+import { vars } from '@/styles/vars'
 
-export const CenterContainer = styled.div`
-	${flex('row', 'center', 'center')}
+interface ICenterContainerProps {
+	absolutePosition?: boolean
+}
+
+const absolute = `
+	padding: 0;
 	position: absolute;
 	top: 0;
 	right: 0;
 	bottom: 0;
 	left: 0;
+`
+
+export const CenterContainer = styled.div<ICenterContainerProps>`
+	${flex('row', 'center', 'center')}
+	padding: 0 0;
+
+	${({ absolutePosition = false }) =>
+		absolutePosition && absolute}
+
+	@media screen and (min-width: ${vars.breakpoints.tablet}){
+		/* ${absolute} */
+	}
 `
