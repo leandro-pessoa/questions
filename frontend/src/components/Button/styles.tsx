ad60d0682b769471f500f132f5146bfaa93dd70e
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-20 16:03:19 -0300

    feat(components): add icon prop styles on the button

--- frontend/src/components/Button/styles.tsx
+++ frontend/src/components/Button/styles.tsx
@@ -3,23 +3,35 @@ import { vars } from '@/styles/vars'
 import { flex } from '@/utils/flex'
 
 interface IStyledButtonProps {
-	backgroundColor?: string
+	readonly $backgroundColor?: string
+	readonly $iconButton?: boolean
 }
 
 export const StyledButton = styled.button<IStyledButtonProps>`
 	${flex('row', 'center', 'center', '6px')}
-	background-color: ${(props) => props.backgroundColor || vars.colors.blue};
-	color: ${(props) => props.backgroundColor ? props.theme.colors.primaryFontColor : vars.colors.white};
+	background-color: ${(props) => props.$backgroundColor || vars.colors.blue};
+	color: ${(props) => props.$backgroundColor ? props.theme.colors.primaryFontColor : vars.colors.white};
+	color: ${(props) => props.$iconButton && props.theme.colors.primaryFontColor};
 	border-radius: ${vars.border.radius};
 	padding: 8px 12px;
 	border: none;
 
 	&:hover {
-		background-color: ${(props) => props.backgroundColor || vars.colors.blue}dd;
+		background-color: ${(props) => props.$backgroundColor || vars.colors.blue}dd;
 		cursor: pointer;
 	}
 
 	&:active {
 		transform: translate(.5px, .5px);
 	}
+
+	${({ $iconButton = false }) => $iconButton && `
+		background-color: transparent;
+		border-radius: 50%;
+		padding: 6px;
+
+		&:hover {
+			background-color: ${vars.colors.gray}3;
+		}
+	`}
 `
