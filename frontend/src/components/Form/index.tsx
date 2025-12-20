f0074d0bd828383995fede9297d8ca209d1ab437
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-20 16:00:47 -0300

    feat(components): add grid layout prop on the form

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -1,22 +1,36 @@
 import styled from 'styled-components'
 import { FormProvider, useForm, type FieldValues } from 'react-hook-form'
+import { vars } from '@/styles/vars'
 
 interface IFormProps {
 	children: React.ReactNode | string | [React.ReactNode | string][]
 	onSubmit: (data: FieldValues) => void
+	grid?: boolean
 }
 
-const StyledForm = styled.form`
+interface IStyledFormProps {
+	readonly $grid?: boolean
+}
+
+const StyledForm = styled.form<IStyledFormProps>`
 	display: flex;
 	flex-direction: column;
+
+	${({ $grid = false }) => $grid && `
+		@media screen and (min-width: ${vars.breakpoints.tablet}) {
+			display: grid;
+			grid-template-columns: auto auto;
+			gap: 16px;
+		}
+	`}
 `
 
-const Form = ({ children, onSubmit }: IFormProps) => {
+const Form = ({ children, onSubmit, grid = false }: IFormProps) => {
 	const methods = useForm()
 
 	return (
 		<FormProvider {...methods}>
-			<StyledForm onSubmit={methods.handleSubmit(onSubmit)}>
+			<StyledForm onSubmit={methods.handleSubmit(onSubmit)} $grid={grid}>
 				{children}
 			</StyledForm>
 		</FormProvider>
