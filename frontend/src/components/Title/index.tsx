a1ef9f8f162ccd9fb488968ff2e370c39e2dcdf4
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-20 18:06:29 -0300

    fix(components): change title size for small devices

--- frontend/src/components/Title/index.tsx
+++ frontend/src/components/Title/index.tsx
@@ -2,11 +2,7 @@ import { vars } from '@/styles/vars'
 import styled from 'styled-components'
 
 export const Title = styled.h1`
-	font-size: large;
-
-	@media screen and (min-width: ${vars.breakpoints.smartphone}) {
-		font-size: x-large;
-	}
+	font-size: x-large;
 
 	@media screen and (min-width: ${vars.breakpoints.notebook}) {
 		font-size: xx-large;
