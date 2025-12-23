d0eeaf72072eaf7253f1228e0ed92076676c019b
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-22 23:06:34 -0300

    fix(components): change height and background of header

--- frontend/src/components/Header/styles.tsx
+++ frontend/src/components/Header/styles.tsx
@@ -1,9 +1,11 @@
 import { flex } from '@/utils/flex'
 import styled from 'styled-components'
+import { vars } from '@/styles/vars'
 
 export const StyledHeader = styled.header`
 	${flex('row', 'space-around', 'center')}
-	height: 60px;
-	background-color: ${(props) => props.theme.colors.secondaryBackgroundColor};
+	height: calc(${vars.sizes.headerHeight} - 1px);
+	background-color: transparent;
+	backdrop-filter: blur(10px);
 	border-bottom: 1px solid ${(props) => props.theme.colors.primaryBorderColor};
 `
