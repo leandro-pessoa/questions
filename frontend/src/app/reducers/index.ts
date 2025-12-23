cbeb2b2754ce3cfcdc4dfc034b15604c9a7914e8
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-22 23:08:29 -0300

    feat(reducers): add questions reducers in the root reducer

--- frontend/src/app/reducers/index.ts
+++ frontend/src/app/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import themeReducer from './theme'
 import loadingReducer from './loading'
 import userReducer from './user'
+import questionReducer from './question'
 
 export const rootReducer = combineReducers({
 	theme: themeReducer,
 	loading: loadingReducer,
-	user: userReducer
+	user: userReducer,
+	question: questionReducer
 })
