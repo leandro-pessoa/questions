94f90ec3a9f47deb90c8a7977340e8218f51d340
Leandro Pessoa <leandropessoareis@gmail.com>
2025-12-31 15:22:29 -0300

    feat(components): add a quesiton header and update option

--- frontend/src/components/Question/index.tsx
+++ frontend/src/components/Question/index.tsx
@@ -1,21 +1,11 @@
 import { StyledLi } from './styles'
 import type { IQuestion } from '@/types/IQuestion'
-import { Small } from '../Small'
 import Button from '../Button'
 import Option from './Option'
 import { useState } from 'react'
 
-// subject
-// statement
-// year?
-// instituition?
-// position?
-// examiningBoard?
-// wrongAlternatives
-// rightAlternative
-
 interface IQuestionProps {
-	randomAlternatives: string[] | number[]
+	index: number
 }
 
 const Question = ({
@@ -25,40 +15,41 @@ const Question = ({
 	instituition = '',
 	position = '',
 	examiningBoard = '',
-	randomAlternatives
+	alternatives,
+	index
 }: IQuestion & IQuestionProps) => {
 	const [selectedOption, setSelectedOption] = useState<number | null>(null)
 
-	const multipleAlternatives = ['A', 'B', 'C', 'D', 'E']
-	const twoAlternatives = ['C', 'E']
-
 	return (
 		<StyledLi>
-			<Small>{subject}</Small>
-			<Small>{instituition}</Small>
-			<Small>{position}</Small>
-			<Small>
-				({examiningBoard} -{' '}
-				<Small style={{ display: 'inline' }}>{year}</Small>)
-			</Small>
-			<span>{statement}</span>
-			<ol className='alternatives'>
-				{randomAlternatives.map((alternative, index) => {
+			<p className='question__header'>
+				<span className='header__number'>{index + 1}</span>
+				<span className='header__subject header__element'>{subject}</span>
+				<span className='header__element'>
+					({examiningBoard} -{' '}
+					<p style={{ display: 'inline' }}>{year}</p>)
+				</span>
+				<span className='header__element'>{instituition}</span>
+				<span className='header__element'>{position}</span>
+			</p>
+			<p className='question__statement'>{statement}</p>
+			<ol className='question__alternatives'>
+				{alternatives.map((alternative, index) => {
 					return (
 						<Option
 							alternative={alternative}
-							index={index}
-							multipleAlternatives={multipleAlternatives}
-							randomAlternatives={randomAlternatives}
-							twoAlternatives={twoAlternatives}
 							selected={selectedOption === index}
-							setSelected={() => setSelectedOption(index)}
-							key={alternative}
+							setSelected={(option) => setSelectedOption(option)}
+							key={alternative._id}
+							index={index}
 						/>
 					)
 				})}
 			</ol>
-			<Button style={{ marginTop: '16px' }}>Responder</Button>
+			{
+				selectedOption !== null &&
+					<Button style={{ marginTop: '16px' }}>Responder</Button>
+			}
 		</StyledLi>
 	)
 }
