e770002006ca6700f07902abf9267407ebd059de
Leandro Pessoa <leandropessoareis@gmail.com>
2026-01-27 23:24:14 -0300

    fix(components): add responsiveness to selects grid layout

--- frontend/src/components/Filters/styles.tsx
+++ frontend/src/components/Filters/styles.tsx
@@ -14,12 +14,15 @@ export const StyledSection = styled.section<IStyledSectionProps>`
 	padding: 0 5%;
 
 	.filters__content {
-		display: grid;
-		grid-template-columns: 32% 32% 32%;
-		gap: 8px;
 		padding: ${(props) => props.$display ? '16px' : '0'} 0;
 		height: ${(props) => props.$display ? 'auto' : '0'};
 		overflow: ${(props) => props.$display ? 'visible' : 'hidden'};
+
+		.content__selects {
+			display: grid;
+			grid-template-columns: 100%;
+			gap: 8px;
+		}
 
 		.content__filter-button {
 			grid-column: 1;
@@ -28,8 +31,22 @@ export const StyledSection = styled.section<IStyledSectionProps>`
 		}
 	}
 
+	@media screen and (min-width: ${vars.breakpoints.smallSmartphone}) {
+		.filters__content {
+			.content__selects {
+				grid-template-columns: 49% 49%;
+			}
+		}
+	}
+
 	@media screen and (min-width: ${vars.breakpoints.tablet}) {
 		padding: 0 10%;
+
+		.filters__content {
+			.content__selects {
+				grid-template-columns: 32% 32% 32%;
+			}
+		}
 	}
 
 	@media screen and (min-width: ${vars.breakpoints.desktop}) {
