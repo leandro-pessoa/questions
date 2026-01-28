b176584971e338175432ee212d67a165de145305
Leandro Pessoa <leandropessoareis@gmail.com>
2026-01-27 22:09:34 -0300

    fix(components): transform Checkbox into a generic component

--- frontend/src/components/Checkbox/index.tsx
+++ frontend/src/components/Checkbox/index.tsx
@@ -2,28 +2,20 @@ import { StyledLabel } from './styles'
 
 interface ICheckboxProps {
 	label: React.ReactNode | string | [React.ReactNode | string][]
-	topics: string[]
-	setSelectedTopics: (topic: string[]) => void
-	value: string
+	checkHandle: () => void
+	checked: boolean
 }
 
-const Checkbox = ({ label, setSelectedTopics, topics, value }: ICheckboxProps) => {
-	// irá atuar quando a checkbox for alterada
-	const checkHandle = () => {
-		// caso o valor já esteja na lista, irá remover por meio de um filter
-		if(topics.includes(value)) {
-			setSelectedTopics(topics.filter((topic) => topic !== value))
-			return
-		}
-
-		// caso não, adicionará o valor na lista
-		setSelectedTopics([...topics, value])
-	}
-
+const Checkbox = ({ label, checkHandle, checked }: ICheckboxProps) => {
 	return (
 		<StyledLabel>
 			{label}
-			<input type='checkbox' className='checkbox__input' onChange={checkHandle}/>
+			<input
+				type='checkbox'
+				className='checkbox__input'
+				onChange={checkHandle}
+				checked={checked}
+			/>
 			<span className='checkbox__checkmark'></span>
 		</StyledLabel>
 	)
