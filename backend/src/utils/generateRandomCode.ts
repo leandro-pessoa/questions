ab6efb2e9447034156bd0c5ea8128d5d68251c5f
Leandro Pessoa <leandropessoareis@gmail.com>
2026-03-08 21:46:21 -0300

    chore(backend): add comments in generateRandomCode util func

--- backend/src/utils/generateRandomCode.ts
+++ backend/src/utils/generateRandomCode.ts
@@ -1,3 +1,4 @@
+// retorna um código aleatório com a quantidade de dígitos informada no parâmetro
 export const generateRandomCode = (length: number) => {
 	return (Math.random() * 1000000000000000).toFixed(0).substring(0, length)
 }
