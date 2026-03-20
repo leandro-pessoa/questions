db6e5ad63745a5c61fb22d764a84137eabc1c5db
Leandro Pessoa <leandropessoareis@gmail.com>
2026-03-19 21:24:17 -0300

    feat: add express-delay to server app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,15 @@ import errors from './middlewares/errors'
 import cors from 'cors'
 import { corsOptions } from './config/corsOptions'
 import helmet from 'helmet'
+import delay from 'express-delay'
 
 // conexão com o banco de dados
 connectDatabase().then(() => { console.log('Conectado ao banco de dados')})
 
 const app = express()
+
+// delay para testar o loading do frontend
+app.use(delay(1000))
 
 // utilização do cors em todas as rotas
 app.use(cors(corsOptions))
