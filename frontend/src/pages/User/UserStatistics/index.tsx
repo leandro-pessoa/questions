b952808b39e5c6b1be5349ee5aa069e0d449049d
Leandro Pessoa <leandropessoareis@gmail.com>
2026-04-01 23:05:36 -0300

    fix(pages): remove useEffect condition and add comments at UserStatistics

--- frontend/src/pages/User/UserStatistics/index.tsx
+++ frontend/src/pages/User/UserStatistics/index.tsx
@@ -20,7 +20,10 @@ type IUserAnsweredQuestions = {
 const UserStatistics = () => {
 	const dispatch = useAppDispatch()
 
+	// token de sessão do user
 	const token = useAppSelector(selectToken)
+
+	// questões respondidas do user (definidas pela resposta da requisição)
 	const [userAnsweredQuestions, setUserAnsweredQuestions] =
 		useState<IUserAnsweredQuestions>({
 			answeredQuestions: [],
@@ -28,30 +31,38 @@ const UserStatistics = () => {
 			incorrect: 0
 		})
 
+	// state do loading local
 	const [loading, setLoading] = useState<boolean>(false)
 
+	// configuração do chartjs
 	ChartJS.register(ArcElement, Tooltip, Legend)
 
 	useEffect(() => {
+		// obtém as questões respondidas do user da API
 		const getUserQuestions = async () => {
 			try {
+				// loading
 				setLoading(true)
+				// requisição, acrescentando o token de sessão do user
 				const res = await http.get(
 					'/users/getAnsweredQuestions',
 					{ headers: { Authorization: token && `Bearer ${token}`}}
 				)
+				// definição do state com base na resposta
 				setUserAnsweredQuestions(res.data)
-			} catch(err) {
+			} catch(err) { // catch do error
 				axiosError(err)
 			}
+			// loading
 			setLoading(false)
 		}
 
-		if (userAnsweredQuestions.answeredQuestions.length < 1) getUserQuestions()
-	}, [token, dispatch, userAnsweredQuestions.answeredQuestions.length])
+		// execução da função de requisição
+		getUserQuestions()
+	}, [token, dispatch])
 
-	console.log(userAnsweredQuestions)
-
+	// configuração do gráfico que será exibido
+	// legendas, dados e cores
 	const data = {
 		labels: ['Acertos', 'Erros'],
 		datasets: [
@@ -68,17 +79,17 @@ const UserStatistics = () => {
 
 	return (
 		<>
-			{
+			{	// caso o state loading seja true, exibe o componente de loading
 				loading ?
 					<Loading $overlay={false}><div></div></Loading>
 				:
 					<>
-						{
+						{	// caso nenhuma questão for respondida, exibe uma mensagem
 							userAnsweredQuestions.answeredQuestions.length < 1 ?
 								<p style={{ textAlign: 'center', margin: '32px 0' }}>
 									Nenhuma questão foi respondida ainda.
 								</p>
-							:
+							:  	// caso exista, exibe o gráfico com as configurações
 								<Pie data={data} />
 						}
 					</>
