88065642da7eee00bfdadb244391a50d3b62d4dc
Leandro Pessoa <leandropessoareis@gmail.com>
2026-04-07 23:16:36 -0300

    feat(pages): add back button to User

--- frontend/src/pages/User/index.tsx
+++ frontend/src/pages/User/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom'
+
 import { CenterContainer } from '@/components/CenterContainer'
 import { Container } from '@/components/Container'
 import Header from '@/components/Header'
@@ -5,13 +7,20 @@ import { Title } from '@/components/Title'
 import UpdateUser from './UpdateUserData'
 import UpdateUserPassword from './UpdateUserPassword'
 import UserStatistics from './UserStatistics'
-
+import Button from '@/components/Button'
+import { ArrowLeft } from 'lucide-react'
 
 const User = () => {
+	const navigate = useNavigate()
+
 	return (
 		<>
 			<Header />
 			<CenterContainer style={{ gap: '32px', padding: '48px 0' }}>
+				<Button onClick={() => navigate(-1)}>
+					<ArrowLeft />
+					Voltar
+				</Button>
 				<Container>
 					<Title>Estatísticas</Title>
 					<UserStatistics />
