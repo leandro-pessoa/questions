aaaa404cbec81927bd62c5fad8ea6dbeed0f631e
Leandro Pessoa <leandropessoareis@gmail.com>
2026-04-27 23:49:52 -0300

    feat(components): update Crud styles with new filters section

--- frontend/src/components/Crud/styles.tsx
+++ frontend/src/components/Crud/styles.tsx
@@ -1,36 +1,62 @@
 import { vars } from '@/styles/vars'
+import { flex } from '@/utils/flex'
 import styled from 'styled-components'
 
 export const StyledDiv = styled.div`
-	max-height: 70vh;
-	max-width: 80%;
-	overflow: auto;
+	${flex('column', 'auto', 'auto', '16px')}
+	width: 95%;
 	margin: 32px 0;
 
-	table {
-		border-radius: 10px;
-
-		th {
-			border-bottom: 2px solid ${vars.colors.blue};
+	.filters_wrapper {
+		${flex('row', 'space-between', 'center', '8px')}
+		.filters_wrapper__search_input {
+			width: 100px;
 		}
+	}
 
-		th, td {
-			padding: 12px;
-			text-align: center;
+	.responsive_table {
+		max-height: 70vh;
+		width: 100%;
+		overflow: auto;
+
+		table {
+			width: 100%;
+			border-radius: 10px;
+
+			th {
+				border-bottom: 2px solid ${vars.colors.blue};
+			}
+
+			th, td {
+				padding: 12px;
+				text-align: center;
+			}
+
+			td {
+				border-bottom: 2px solid ${(props) => props.theme.colors.primaryBorderColor};
+				max-width: 100px;
+				text-overflow: ellipsis;
+				white-space: nowrap;
+				overflow: hidden;
+			}
 		}
+	}
 
-		td {
-			border-bottom: 2px solid ${(props) => props.theme.colors.primaryBorderColor};
-			max-width: 100px;
-			text-overflow: ellipsis;
-			white-space: nowrap;
-			overflow: hidden;
+	@media screen and (min-width: ${vars.breakpoints.smallSmartphone}) {
+		.filters_wrapper {
+			.filters_wrapper__search_input {
+				width: max-content;
+			}
 		}
 	}
 
 	@media screen and (min-width: ${vars.breakpoints.smartphone}) {
+		width: 80%;
 		margin: 0;
-		max-height: 80vh;
+
+		.responsive_table {
+			max-height: 80vh;
+		}
 	}
 `
 
