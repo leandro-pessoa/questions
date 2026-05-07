c9a012b4ad1f2896113f2a812cb331d72f421786
Leandro Pessoa <leandropessoareis@gmail.com>
2026-05-06 23:11:18 -0300

    feat(components): create Modal

---
+++ frontend/src/components/Modal/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from 'react'
+import { selectModalDisplay, setModalDisplay } from '@/app/reducers/modal'
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+
+import { CenterContainer } from '../CenterContainer'
+import { Container } from '../Container'
+import { Hr } from '../Hr'
+import { Title } from '../Title'
+import { StyledDiv } from './styles'
+import Button from '../Button'
+
+import type { ReactChildren } from '@/types/ReactChildren'
+
+interface ModalProps {
+	title: string
+	children: ReactChildren
+	closeElement: ReactChildren
+	execButton: ReactChildren
+}
+
+const Modal = ({ title, children, closeElement, execButton }: ModalProps) => {
+	const dispatch = useAppDispatch()
+
+	const modalDisplay = useAppSelector(selectModalDisplay)
+
+	const ref = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		// fecha o modal ao clicar fora do container central
+        const handleClickOutside = (event: MouseEvent) => {
+            const target = event.target as HTMLElement
+            if (ref.current && !ref.current.contains(target)) {
+                dispatch(setModalDisplay(false))
+            }
+        }
+        document.addEventListener('click', handleClickOutside, true)
+	}, [dispatch])
+
+	const activeDisplay = modalDisplay ? 'block' : 'none'
+
+	return (
+		<StyledDiv style={{ display: activeDisplay }}>
+			<CenterContainer $height='center-fixed'>
+				<Container
+					$backgroundColor='colorful'
+					$relativeWidth='50%'
+					style={{ gap: '8px', display: activeDisplay }}
+					ref={ref}
+				>
+					<Title>{title}</Title>
+					<Hr />
+					{children}
+					<div className='buttons_wrapper'>
+						{execButton}
+						<Button onClick={() => dispatch(setModalDisplay(false))}>
+							{closeElement}
+						</Button>
+					</div>
+				</Container>
+			</CenterContainer>
+		</StyledDiv>
+	)
+}
+
+export default Modal
---
+++ frontend/src/components/Modal/styles.tsx
@@ -0,0 +1,19 @@
+import { flex } from '@/utils/flex'
+import styled from 'styled-components'
+
+export const StyledDiv = styled.div`
+	position: fixed;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	backdrop-filter: blur(4px);
+	z-index: 13;
+
+	.buttons_wrapper {
+		${flex('row', 'auto', 'center', '16px')}
+		margin-top: 16px;
+	}
+`
+
+
