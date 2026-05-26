a02cc66e6f8dd80c85573ce078fd9ba75845111e
Leandro Pessoa <leandropessoareis@gmail.com>
2026-05-25 21:49:04 -0300

    fix(pages): update EditQuestions layout

--- frontend/src/pages/Admin/QuestionsPage/EditQuestion/styles.tsx
+++ frontend/src/pages/Admin/QuestionsPage/EditQuestion/styles.tsx
@@ -2,46 +2,71 @@ import { vars } from '@/styles/vars'
 import { flex } from '@/utils/flex'
 import styled from 'styled-components'
 
-export const StyledUl = styled.ul`
-	width: 100%;
+export const AlternativesDiv = styled.div`
+	grid-column: 1 / 3;
 
-	li {
-		${flex('row', 'auto', 'flex-start', '8px')}
-		padding: 8px 0;
+	ul {
+		width: 100%;
 
-		.alternative__letter-container {
-			width: 20%;
-
-			input {
-				width: calc(100% - 12px);
-			}
-		}
-
-		.alternative__text-container {
-			width: 80%;
-		}
-
-		.alternavite__options {
-			${flex('row', 'auto', 'center', '6px')}
-		}
-	}
-
-	@media screen and (min-width: ${vars.breakpoints.smartphone}) {
 		li {
-			padding: 8px 16px;
+			${flex('row', 'auto', 'flex-start', '8px')}
+			padding: 8px 0;
 
 			.alternative__letter-container {
 				width: 20%;
+
+				input {
+					width: calc(100% - 12px);
+				}
 			}
 
 			.alternative__text-container {
-				width: 90%;
+				width: 80%;
+			}
+
+			.alternavite__options {
+				${flex('row', 'auto', 'center', '6px')}
+			}
+		}
+
+		@media screen and (min-width: ${vars.breakpoints.smartphone}) {
+			li {
+				padding: 8px 16px;
+
+				.alternative__letter-container {
+					width: 20%;
+				}
+
+				.alternative__text-container {
+					width: 90%;
+				}
+			}
+		}
+	}
+
+	.empty-alternatives {
+		text-align: center;
+		margin: 16px 0;
+	}
+
+	.add-alternative {
+		margin-top: 16px;
+
+		svg {
+			height: 15px;
+		}
+	}
+
+	@media screen and (min-width: ${vars.breakpoints.tablet}) {
+		.add-alternative {
+			svg {
+				height: 20px;
 			}
 		}
 	}
 `
 
-export const StyledDiv = styled.div`
+export const ButtonsDiv = styled.div`
 	${flex('row', 'auto', 'center', '16px')}
 	grid-column: 1 / 3;
 	margin-top: 12px;
