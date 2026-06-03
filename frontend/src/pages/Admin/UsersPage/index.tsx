55a96241b111e43b27c778a115871148f924c3b4
Leandro Pessoa <leandropessoareis@gmail.com>
2026-06-02 22:04:06 -0300

    feat(pages): add form modals on UsersPage

--- frontend/src/pages/Admin/UsersPage/index.tsx
+++ frontend/src/pages/Admin/UsersPage/index.tsx
@@ -3,10 +3,11 @@ import { selectToken } from '@/app/reducers/user'
 import { useEffect, useState } from 'react'
 import { selectActualPage, selectAdminUsersStatus, selectFetchLimit, selectTotalAdminUsersPages } from '@/app/reducers/adminUsers'
 import { fetchAdminUsers, selectAdminUsers } from '@/app/reducers/adminUsers'
-import { selectModalData, setModalData, setModalType } from '@/app/reducers/modal'
+import { selectModalData, selectModalType, setModalData, setModalOverflow, setModalType } from '@/app/reducers/modal'
 
 import Crud from '@/components/Crud'
 import RemoveUser from './RemoveUser'
+import UserForm from './UserForm'
 
 import type { IUser } from '@/types/IUser'
 
@@ -20,6 +21,7 @@ const UsersPage = () => {
 	const adminUsersActualPage = useAppSelector(selectActualPage)
 	const adminUsersTotalPages = useAppSelector(selectTotalAdminUsersPages)
 	const modalData = useAppSelector(selectModalData) as IUser
+	const modalType = useAppSelector(selectModalType)
 	// state do loading local
 	const [loading, setLoading] = useState<boolean>(false)
 
@@ -40,9 +42,22 @@ const UsersPage = () => {
 		dispatch(setModalData({ _id, email }))
 	}
 
+	const openEditModal = (user: IUser) => {
+		dispatch(setModalType('editUser'))
+		dispatch(setModalOverflow(true))
+		dispatch(setModalData({...user}))
+	}
+
+	const openAddModal = () => {
+		dispatch(setModalType('addUser'))
+		dispatch(setModalOverflow(true))
+	}
+
 	return (
 		<>
 			<RemoveUser {...modalData}/>
+			{modalType === 'editUser' && <UserForm user={modalData} mode='put' />}
+			{modalType === 'addUser' && <UserForm mode='post' />}
 			<Crud
 				labels={[
 					'ID',
@@ -58,8 +73,9 @@ const UsersPage = () => {
 				actualPage={adminUsersActualPage}
 				limit={adminUsersLimit}
 				totalPages={adminUsersTotalPages}
-				editFunc={() => {}}
+				editFunc={openEditModal}
 				removeFunc={openRemoveModal}
+				addFunc={openAddModal}
 			/>
 		</>
 	)
