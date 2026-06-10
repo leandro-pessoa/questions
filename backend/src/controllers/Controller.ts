cffc1f2213380a20fe2ceccadb166368cf46ba52
Leandro Pessoa <leandropessoareis@gmail.com>
2026-06-09 22:52:33 -0300

    feat(backend): create search controller method for all models

--- backend/src/controllers/Controller.ts
+++ backend/src/controllers/Controller.ts
@@ -1,3 +1,4 @@
+import BadRequest from '@/errors/BadRequest'
 import NotFound from '@/errors/NotFound'
 import CRUDServices from '@/services/CRUDServices'
 import type { Request, Response, NextFunction } from 'express'
@@ -14,6 +15,61 @@ export default class Controller<T> {
 			req.paginationModel = this.serviceEntity.model
 
 			next()
+		} catch (err) {
+			next(err)
+		}
+	}
+
+	// faz uma pesquisa em um model
+	// na query, é possível informar o valor a ser pesquisado e a coluna alvo
+	async searchIndex(req: Request, res: Response, next: NextFunction) {
+		const { searchValue, column } = req.query
+
+		// caso não seja informado o valor ou a coluna
+		if (!searchValue || !column) {
+			next(new BadRequest())
+			return
+		}
+
+		// caso a coluna selecionada seja referente a alguma senha
+		if (column === 'password' || column === 'passwordHash') {
+			next(new BadRequest())
+			return
+		}
+
+		// caso os tipos informados do valor não sejam number ou string
+		// caso o tipo de column não seja string
+		if (
+			(typeof searchValue !== 'string' && typeof searchValue !== 'number')
+			|| typeof column !== 'string'
+		) {
+			next(new BadRequest())
+			return
+		}
+
+		// regex para verificar se o valor é uma sequência de números
+		const isNumber = searchValue.match(/^[0-9]{1,}$/)
+
+		// objeto de pesquisa que será enviado para o find do model
+		const searchObj: Record<string, RegExp | string> = {}
+
+		// regex para o valor de pesquisa
+		const searchRegex = new RegExp(searchValue, 'gi')
+
+		// cria um atributo no objeto searchObj e atribui um valor a ele de acordo com o isNumber
+		searchObj[column] = isNumber ? searchValue : searchRegex
+
+		try {
+			// faz a pesquisa utilizando o service getAll
+			const result = await this.serviceEntity.getAll(searchObj)
+
+			// caso o valor não seja encontrado
+			if (result.length === 0) {
+				next(new NotFound())
+				return
+			}
+
+			return res.status(200).json(result)
 		} catch (err) {
 			next(err)
 		}
