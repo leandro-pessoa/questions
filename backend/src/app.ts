d2ea6b6ca81798da15c96e4d3097d469d649d367
Leandro Pessoa <leandropessoareis@gmail.com>
2026-07-16 23:05:38 -0300

    chore(backend): remove express delay at app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,16 +5,12 @@ import errors from './middlewares/errors'
 import cors from 'cors'
 import { corsOptions } from './config/corsOptions'
 import helmet from 'helmet'
-import delay from 'express-delay'
 import { limiter } from './middlewares/limiter'
 
 // conexão com o banco de dados
 connectDatabase().then(() => { console.log('Conectado ao banco de dados')})
 
 const app = express()
-
-// delay para testar o loading do frontend
-app.use(delay(1000))
 
 // utilização do cors em todas as rotas
 app.use(cors(corsOptions))
