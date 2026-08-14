5d84f1fd6026b437d26307edf8720095c9d82438
Leandro Pessoa <leandropessoareis@gmail.com>
2026-08-13 21:26:43 -0300

    feat(backend): update database connection string

--- backend/src/db/connectDatabase.ts
+++ backend/src/db/connectDatabase.ts
@@ -5,7 +5,7 @@ export const connectDatabase = async (): Promise<
 > => {
 	try {
 		// utiliza a string de conexão do mongoose que precisa ser informada no arquivo .env
-		mongoose.connect(process.env.DATABASE_CONNECTION_STRING ?? '')
+		mongoose.connect((process.env.CLOUD_DATABASE_CONNECTION_STRING || process.env.LOCAL_DATABASE_CONNECTION_STRING) ?? '', { authSource: 'admin' })
 		return mongoose.connection
 	} catch (err) {
 		console.log(err)
